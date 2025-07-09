cf395525214fc693bd2c5f008f8ecee6915ef827
idembele70 <idembele70@gmail>
2025-07-10 01:45:29 +0200

    feat: wip

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js },
+    extends: [
+      "js/recommended",
+      ...tseslint.configs.recommended,
+      ...pluginReact.configs.flat.recommended,
+
+    ]
+  },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+
+  {}
+]);
